22a1950c83e8857cca05ce8470f89a76e4d088fa
ABC <a@b.com>
2024-10-13 10:15:28 +0530

    Looping Discussion

---
+++ looping/exercises.py
@@ -0,0 +1,62 @@
+# WAP to generate a multiplication table for user provided number.
+
+# O/P
+# 2 * 1 = 2
+# 2 * 2 = 4
+# 2 * 3 = 6
+#.
+#.
+# 2 * 10 = 20
+
+'''
+num = int(input("Enter the number : "))
+terms = int(input("Please enter the number of terms to be printed: "))
+
+i = 1
+while i<=terms:
+    prod = num * i
+    print(num, '*', i, '=', prod)
+    i = i + 1
+
+
+
+for i in range(1, terms+1):
+    prod = num * i
+    print(num, '*', i, '=', prod)
+
+'''
+
+# WAP to get number from user and give Factorial
+# !5 = 5 * 4 * 3 * 2 * 1
+# O/P : 120
+
+
+num = int(input("Enter the number : "))
+
+fact = 1
+while (num >=1):
+    fact = fact * num
+    num = num - 1
+
+print("Factorial :", fact)
+
+
+num = int(input("Enter the number : "))
+fact = 1
+for x in range(1, num+1):
+    fact = fact * x
+
+print("Factorial From For :", fact)
+
+
+fact = 1
+for x in range(5, 0, -1):
+    fact = fact * x
+
+print("Factorial (For Reverse) :", fact)
+
+
+# WAP to generate fibonacci series. Take number of terms from user
+# 0  1  1  2  3  5  8  13  21  34 .....
+
+# input : n1 = 0,  n2 = 1
--- looping/looping.py
+++ looping/looping.py
@@ -76,6 +76,7 @@ print("Sum : ", sum)
 #numbers = [1, 2, 3, 4, 5, 6]  # No of elements in list = size of list = 6
 numbers = [1, 2, 3, 4, 5, 6, 5, 6, 7, 8]
 
+# while
 print(type(numbers))
 print("No of elements:", len(numbers))
 
@@ -90,7 +91,45 @@ print(sum)
 
 
 
+# for
+sum = 0
+for num in numbers:
+    sum = sum + num
+
+print("Sum:", sum)
 
 
+# numbers = [1, 2, 3, 4, 5, 6, 5, 6, 7, 8]
 
 
+# range(start, stop, [step]) => generator function => generate => sequence of numbers
+
+# range(1, 10) => iterable => for/while => get the sequence
+
+print(range(1,11))
+print(type(range(1,11)))
+
+for num in range(1,11):
+    print(num)
+
+
+# positional argument in for loop
+
+numbers = [1, 2, 3, 4, 5, 6, 5, 6, 7, 8] # startIndex = 0 , endIndex = 9
+sum = 0
+for pos in range(0, len(numbers)):
+    sum = sum + numbers[pos]
+
+print("Sum from for loop : ", sum)
+
+
+# range([start], stop, [step]) : default => start : 0 , step: 1
+# range(1, 10) <=> range(1, 10, 1)
+# range(10) <=> range(0,10) <=> range(0,10,1)
+
+for x in range(10):
+    print(x)
+
+# -ve sequence
+for num in range(11,0,-1):
+    print(num)
